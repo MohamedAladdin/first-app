2b193186d933fd5291bccb7ae5f975fd7662fd16
MohamedAladdin <mohamed_aladdin@live.com>
2022-08-09 03:14:28 +0200

    My first steps in the React.JS Course

---
+++ src/footer.js
@@ -0,0 +1,21 @@
+const Footer = () => {
+    return ( 
+
+        <footer> 
+
+            <div className='head'> FOOTER </div> 
+
+            <div className='ads'> 
+                <p> This is An AD </p>
+            </div>    
+
+            <div className='copyrights'> 
+                <p> Copyrights reserved 2018/2025 </p>
+            </div>
+            
+        </footer>
+
+    );
+}
+ 
+export default Footer;
---
+++ src/header.js
@@ -0,0 +1,19 @@
+const Header = () => {
+    return ( 
+
+        <ul className='header' >
+            
+            <li> Mohamed </li>
+            <li> Sayed </li>
+            <li> Ali </li>
+            <li> Hossam </li>
+            <li> Fawzy </li>
+
+        </ul> 
+
+    );
+}
+    
+
+
+export default Header;
